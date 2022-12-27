23d55f97375874fdeabb1cba0e3c337aef304c8d
Kalyshchuk <kalyshchuk89@ukr.net>
2022-12-28 00:22:18 +0200

    7

--- js/menu.js
+++ js/menu.js
@@ -1,26 +1,14 @@
 (() => {
-    const menuBtnRef = document.querySelector("[data-menu-button]");
-    const mobileMenuRef = document.querySelector("[data-menu]");
-//   const openMenuBtn = document.querySelector('.js-open-menu');
-//   const closeMenuBtn = document.querySelector('.js-close-menu');
+  const refs = {
+    openMenuBtn: document.querySelector(".button-menu"),
+    closeMenuBtn: document.querySelector(".main-nav__menu__cross"),
+    menu: document.querySelector(".main-nav__menu"),
+  };
 
-    menuBtnRef.addEventListener("click", () => {
-        const expanded =
-            menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-        
-        menuBtnRef.classList.toggle("is-open");
-        menuBtnRef.setAttribute("aria-expanded", !expanded);
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
 
-        mobileMenuRef.classList.toggle("is-open");
-    })
-
-  // Закрываем мобильное меню на более широких экранах
-  // в случае изменения ориентации устройства.
-    
-//   window
-//     .matchMedia('(min-width: 600px)')
-//     .addEventListener(
-//       'change',
-//       e => e.matches && menuContainer.classList.remove('is-open'),
-//     );
-// })();
+  function toggleMenu() {
+    refs.menu.classList.toggle("is-hidden");
+  }
+})();
